f767313e2ec4f9f274c53677baf133262201cb73
Ghassen Chaabouni <ghassen1302@live.com>
2020-04-19 02:20:38 +0100

    Update person.js

--- Stickman/person.js
+++ Stickman/person.js
@@ -33,36 +33,29 @@ function Person() {
   
 
   this.show = function() {
-	
-    strokeWeight(15);
-	
-   stroke(0);
-   line(this.leftShoulder_x, this.leftShoulder_y, this.rightShoulder_x, this.rightShoulder_y);
-   line(this.rightElbow_x, this.rightElbow_y, this.rightShoulder_x, this.rightShoulder_y);
-   line(this.leftElbow_x, this.leftElbow_y, this.leftShoulder_x, this.leftShoulder_y);
-   line(this.leftWrist_x, this.leftWrist_y, this.leftElbow_x, this.leftElbow_y);
-   line(this.rightWrist_x, this.rightWrist_y, this.rightElbow_x, this.rightElbow_y);
-   line(this.leftHip_x, this.leftHip_y, this.leftShoulder_x, this.leftShoulder_y);
-   line(this.rightHip_x, this.rightHip_y, this.rightShoulder_x, this.rightShoulder_y);
-   line(this.leftHip_x, this.leftHip_y, this.rightHip_x, this.rightHip_y);
-   line(this.leftKnee_x, this.leftKnee_y, this.leftHip_x, this.leftHip_y);
-   line(this.rightKnee_x, this.rightKnee_y, this.rightHip_x, this.rightHip_y);
-  
-   
-   stroke("red");
-    line(this.leftShoulder_x, this.leftShoulder_y, this.leftShoulder_x, this.leftShoulder_y);
-	line(this.rightShoulder_x, this.rightShoulder_y, this.rightShoulder_x, this.rightShoulder_y);
-	line(this.leftWrist_x, this.leftWrist_y, this.leftWrist_x, this.leftWrist_y);
-	line(this.rightWrist_x, this.rightWrist_y, this.rightWrist_x, this.rightWrist_y);
-	line(this.leftHip_x, this.leftHip_y, this.leftHip_x, this.leftHip_y);
-	line(this.rightHip_x, this.rightHip_y, this.rightHip_x, this.rightHip_y);
-	line(this.leftKnee_x, this.leftKnee_y, this.leftKnee_x, this.leftKnee_y);
-	line(this.rightKnee_x, this.rightKnee_y, this.rightKnee_x, this.rightKnee_y);
-	line(this.rightElbow_x, this.rightElbow_y, this.rightElbow_x, this.rightElbow_y);
-	line(this.leftElbow_x, this.leftElbow_y, this.leftElbow_x, this.leftElbow_y);
-	
-  
-   
-   
+	  strokeWeight(15);
+	  stroke(0);
+	  line(this.leftShoulder_x, this.leftShoulder_y, this.rightShoulder_x, this.rightShoulder_y);
+	  line(this.rightElbow_x, this.rightElbow_y, this.rightShoulder_x, this.rightShoulder_y);
+	  line(this.leftElbow_x, this.leftElbow_y, this.leftShoulder_x, this.leftShoulder_y);
+	  line(this.leftWrist_x, this.leftWrist_y, this.leftElbow_x, this.leftElbow_y);
+	  line(this.rightWrist_x, this.rightWrist_y, this.rightElbow_x, this.rightElbow_y);
+	  line(this.leftHip_x, this.leftHip_y, this.leftShoulder_x, this.leftShoulder_y);
+	  line(this.rightHip_x, this.rightHip_y, this.rightShoulder_x, this.rightShoulder_y);
+	  line(this.leftHip_x, this.leftHip_y, this.rightHip_x, this.rightHip_y);
+	  line(this.leftKnee_x, this.leftKnee_y, this.leftHip_x, this.leftHip_y);
+	  line(this.rightKnee_x, this.rightKnee_y, this.rightHip_x, this.rightHip_y);
+	  
+	  stroke("red");
+	  line(this.leftShoulder_x, this.leftShoulder_y, this.leftShoulder_x, this.leftShoulder_y);
+	  line(this.rightShoulder_x, this.rightShoulder_y, this.rightShoulder_x, this.rightShoulder_y);
+	  line(this.leftWrist_x, this.leftWrist_y, this.leftWrist_x, this.leftWrist_y);
+	  line(this.rightWrist_x, this.rightWrist_y, this.rightWrist_x, this.rightWrist_y);
+	  line(this.leftHip_x, this.leftHip_y, this.leftHip_x, this.leftHip_y);
+	  line(this.rightHip_x, this.rightHip_y, this.rightHip_x, this.rightHip_y);
+	  line(this.leftKnee_x, this.leftKnee_y, this.leftKnee_x, this.leftKnee_y);
+	  line(this.rightKnee_x, this.rightKnee_y, this.rightKnee_x, this.rightKnee_y);
+	  line(this.rightElbow_x, this.rightElbow_y, this.rightElbow_x, this.rightElbow_y);
+	  line(this.leftElbow_x, this.leftElbow_y, this.leftElbow_x, this.leftElbow_y);
   };
 }
